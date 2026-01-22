6a109b349a94d00b32f7348b4361328688a74b21
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-01-23 00:02:49 +0600

    Navbar design part is done

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import { NavLink } from "react-router";
 
 const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products">Products</NavLink>
+      </li>
+    </>
+  );
   return (
     <header>
       {/* Search Section Starts */}
       <section className="flex px-8 py-6 justify-between items-center bg-[#111828] text-white font-bold text-xl">
         <NavLink to="/">
-          <img src="" alt="Logo" />
+          <img src="logo.png" alt="SiOi Global Trade Logo" className="h-15" />
         </NavLink>
-        <label className="input lg:w-[55%] hidden md:flex">
+        <label className="input lg:w-[45%] hidden md:flex bg-white text-black rounded-full cursor-pointer hover:outline-2 hover:outline-amber-500">
           <svg
             className="h-[1em] opacity-50"
             xmlns="http://www.w3.org/2000/svg"
@@ -26,11 +39,11 @@ const Navbar = () => {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" required placeholder="Search" />
+          <input type="search" required placeholder="Search Dorin Products" />
         </label>
 
         {/* Contact Info */}
-        <div>
+        <div className="hidden md:block">
           <div className="flex items-center gap-1 text-sm font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -98,83 +111,118 @@ const Navbar = () => {
         </div>
         {/* Contact Info Ends */}
 
-        <ul className="menu menu-horizontal bg-base-200 rounded-box">
+        <ul className="menu menu-horizontal bg-base-200 rounded-box items-center">
           <li>
-            <a className="tooltip" data-tip="Home">
+            <a className="tooltip" data-tip="Favourite Product">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                className="h-5 w-5"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
                 />
               </svg>
             </a>
           </li>
           <li>
-            <a className="tooltip" data-tip="Details">
+            <a className="tooltip" data-tip="Cart">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                className="h-5 w-5"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                 />
               </svg>
             </a>
           </li>
           <li>
-            <a className="tooltip" data-tip="Stats">
+            <a
+              className="tooltip btn btn-soft btn-error ml-2 hidden md:flex"
+              data-tip="Login"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                className="h-5 w-5"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                 />
-              </svg>
+              </svg>{" "}
+              Login
             </a>
           </li>
         </ul>
       </section>
       {/* Search Section Ends */}
       <nav>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? "text-green-600" : "")}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) => (isActive ? "text-green-600" : "")}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/products"
-          className={({ isActive }) => (isActive ? "text-green-600" : "")}
-        >
-          Products
-        </NavLink>
+        <div className="navbar bg-[#bf1d1e] shadow-sm justify-between uppercase font-semibold text-lg px-10">
+          <div className="lg:hidden dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 p-2 shadow"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+          </div>
+          <div className="navbar-end">
+            <a className="btn btn-dash btn-error bg-white rounded-xl font-bold hover:text-[#bf1d1e] hover:transform hover:scale-105 transition-transform duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                />
+              </svg>
+              Track Order
+            </a>
+          </div>
+        </div>
       </nav>
     </header>
   );
